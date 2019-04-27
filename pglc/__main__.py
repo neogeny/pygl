104e659d7626e1da1c498f913832b9e49e9f5dc0
apalala <apalala@gmail.com>
2019-04-27 10:10:00 -0400

    choose appropiate names

--- pglc/__main__.py
+++ pglc/__main__.py
@@ -1,15 +1,13 @@
 import sys
 import json
 
-from . import grammars
-from .parser import pgl_parser
+from .parser import python_grammar_model
 from .util import asjson
 
 
 def main(trace=False):
-    python_grammar = grammars.load_python_grammar()
     try:
-        model = pgl_parser().parse(python_grammar, trace=trace)
+        model = python_grammar_model(trace=trace)
     except Exception as e:
         print(e)
         sys.exit(1)
--- pglc/parser.py
+++ pglc/parser.py
@@ -1,15 +1,19 @@
 import tatsu
 
-from .grammars import load_plg_grammar
+from . import grammars
 from .semantics import PGLSemantics
 
 
 def pgl_parser(trace=False):
-    pglc_grammar = load_plg_grammar()
+    pgl_grammar = grammars.load_plg_grammar()
     return tatsu.compile(
-        pglc_grammar,
+        pgl_grammar,
         semantics=PGLSemantics(),
         parseinfo=True,
         colorize=True,
         trace=trace,
     )
+
+def python_grammar_model(trace=False):
+    python_grammar = grammars.load_python_grammar()
+    return pgl_parser().parse(python_grammar, trace=trace)
