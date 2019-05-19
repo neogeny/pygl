c96ab2b0f78753686b0d1c522f57492c9307aac8
apalala <apalala@gmail.com>
2019-05-19 12:08:55 -0400

    [pglc] implement INDENT/DEDENT

--- pglc/pythonc/semantics.py
+++ pglc/pythonc/semantics.py
@@ -1,7 +1,33 @@
 from dataclasses import dataclass, field
 from typing import List
 
+from tatsu.exceptions import FailedSemantics
+
 
 @dataclass()
 class PythonSemantics:
     indent_levels: List[int] = field(default_factory=list, init=False)
+
+    def error(self, msg):
+        raise FailedSemantics(msg)
+
+    def current_indent(self):
+        return self.indent_levels[-1] if self.indent_levels else 0  # pylint: disable=E1136
+
+    def INDENT(self, ast):
+        print('INDENT "%s"' % ast)
+        indent = len(ast.strip('\r\n'))
+        prev = self.current_indent()
+        if not indent or indent <= prev:
+            return self.error('Expecting INDENT')
+        self.indent_levels.append(indent)
+
+    def DEDENT(self, ast):
+        self.indent_levels.pop()
+
+    def EQDENT(self, ast):
+        print('EQDENT "%s"' % ast)
+        indent = len(ast.strip('\r\n'))
+        prev = self.current_indent()
+        if indent != prev:
+            return self.error('Unexpected change of INDENT')
